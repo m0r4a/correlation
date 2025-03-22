f176c2d59d09aa7963ed4ae51a1f1fbde1a20523
m0r4a <m0r4a@proton.me>
2025-03-22 16:27:55 -0600

    Finish problem 8

---
+++ laboratory/p8.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from numpy import array as nparr
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "functions")))
+
+from table import table
+from graphic import graphic
+from coeficiente_correlacion import calcular_coeficiente_correlacion
+
+x_arr = nparr([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])   # publicaciones
+y_arr = nparr([100, 250, 400, 600, 850, 1100, 1400, 1750, 2150, 2600])   # seguidores
+
+# Variables
+var_ind = "Número de Publicaciones"
+var_dep = "Cantidad de Seguidores Obtenidos"
+titulo_diagrama = "Diagrama de dispersión de la cantidad de Seguidores Obtenidos vs el Número de Publicaciones"
+
+r, conclusion = calcular_coeficiente_correlacion(x_arr, y_arr, var_ind, var_dep)
+
+table("Pearson r value", r, conclusion)
+
+problem_number, _ = sys.argv[0].split(".")
+
+graphic(x_arr, y_arr, var_ind, var_dep, titulo_diagrama, save_path=f"diagramas/{problem_number}_diagrama_dispersion")
